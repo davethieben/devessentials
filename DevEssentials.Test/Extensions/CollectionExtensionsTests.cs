83075f057d6f1cab3d4c27c68a7ca0e44f468dd5
Dave Thieben <davethieben@filevine.com>
2021-04-23 18:41:45 -0400

    add test project

---
+++ DevEssentials.Test/Extensions/CollectionExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Essentials;
+using Xunit;
+
+namespace DevEssentials.Test.Extensions
+{
+    public class CollectionExtensionsTests
+    {
+
+        [Fact]
+        public void IsNullOrEmpty_WhenEmpty()
+        {
+            var collection = Enumerable.Empty<int>();
+            Assert.True(collection.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void IsNullOrEmpty_WhenNull()
+        {
+            int[] collection = null;
+            Assert.True(collection.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void IsNullOrEmpty_WhenNotEmpty()
+        {
+            var collection = new[] { 1 };
+            Assert.False(collection.IsNullOrEmpty());
+        }
+
+    }
+}
