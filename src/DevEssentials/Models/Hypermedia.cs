052feab4b2074866efc7d5d910be0245621e921d
Dave Thieben <davethieben@filevine.com>
2021-05-24 10:14:22 -0400

    updates

--- src/DevEssentials/Models/Hypermedia.cs
+++ src/DevEssentials/Models/Hypermedia.cs
@@ -6,6 +6,11 @@ namespace Essentials.Models
     public class Hypermedia<T> : Hypermedia
         where T : class
     {
+        public Hypermedia(T? model = default)
+        {
+            Model = model;
+        }
+
         public T? Model { get; set; }
     }
 
@@ -14,12 +19,27 @@ namespace Essentials.Models
         public List<HypermediaLink> Links { get; set; } = new List<HypermediaLink>();
         public List<KeyValuePair<string, string>> Errors { get; set; } = new List<KeyValuePair<string, string>>();
 
+        public Hypermedia AddLink(string url, string method = "GET", string? rel = null)
+        {
+            Links.Add(new HypermediaLink(url, rel, method));
+            return this;
+        }
     }
 
     public class HypermediaLink
     {
+        public HypermediaLink(string url, string? rel = null, string? method = null, string? type = null)
+        {
+            Url = url;
+            Rel = rel;
+            if (method != null)
+                Method = method;
+        }
+
         public string Url { get; set; } = default!;
+        public string? Rel { get; set; }
         public string Method { get; set; } = "GET";
+        public string? Type { get; set; }
     }
 
     [AttributeUsage(AttributeTargets.Method)]
