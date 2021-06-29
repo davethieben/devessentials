3b38746967c00a8aafa61146607a92b1d966b934
Dave Thieben <davethieben@filevine.com>
2021-06-29 08:58:28 -0400

    add AutoMapper resolver for repositories

---
+++ src/DevEssentials/Data/IResolverRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Essentials.Data
+{
+    public interface IResolverRepository<T>
+    {
+        T GetById(int id);
+        Task<T> GetByIdAsync(int id);
+    }
+}
---
+++ src/DevEssentials/Helpers/RepositoryResolver.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Essentials.Data;
+
+namespace Essentials.Helpers
+{
+    public class RepositoryResolver<TModel> : IMemberValueResolver<object, object, int, TModel>
+    {
+        private readonly IResolverRepository<TModel> _repository;
+
+        public RepositoryResolver(IResolverRepository<TModel> repository)
+        {
+            _repository = repository;
+        }
+
+        public TModel Resolve(object source, object destination, int sourceId, TModel destMember, ResolutionContext context)
+        {
+            return _repository.GetById(sourceId);
+        }
+    }
+}
