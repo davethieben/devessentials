8da8b54702103868e3e0a84df2aa9516d738ce3f
Dave Thieben <davethieben@filevine.com>
2021-09-10 09:09:34 -0400

    add PagedList from Greta

---
+++ src/DevEssentials/Models/PagedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Essentials.Models
+{
+    public interface IPagedList<T>
+    {
+        T[] Items { get; set; }
+        int TotalCount { get; }
+        int Offset { get; }
+        int PageSize { get; }
+
+        int PageNumber { get; }
+        int StartItem { get; }
+        int EndItem { get; }
+    }
+
+    public class PagedList<T> : IPagedList<T>
+    {
+        public PagedList()
+        {
+            TotalCount = 0;
+            Offset = 0;
+            PageSize = 20;
+        }
+
+        public PagedList(IEnumerable<T> pageItems, int totalCount, int pageNumber = 1, int pageSize = 20)
+        {
+            Items = pageItems.ToArray();
+            TotalCount = totalCount;
+
+            if (pageNumber < 1)
+                pageNumber = 1;
+            Offset = (pageNumber - 1) * pageSize;
+
+            if (pageSize < 1)
+                pageSize = 1;
+            PageSize = pageSize;
+        }
+
+        public T[] Items { get; set; } = default!;
+
+        public int TotalCount { get; }
+
+        public int Offset { get; }
+
+        public int PageSize { get; }
+
+        public int NumPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+
+        public int PageNumber => (int)Math.Ceiling((double)Offset / PageSize);
+
+        public int StartItem
+        {
+            get
+            {
+                int n = Offset + 1;
+                if (n > TotalCount)
+                    return TotalCount;
+                return n;
+            }
+        }
+
+        public int EndItem
+        {
+            get
+            {
+                int n = Offset + PageSize;
+                if (n > TotalCount)
+                    return TotalCount;
+                return n;
+            }
+        }
+
+    }
+}
---
+++ test/DevEssentials.Test/Models/PagedListTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Essentials.Models;
+using Xunit;
+
+namespace Essentials.Test.Models
+{
+    public class PagedListTests
+    {
+
+        [Fact]
+        public void HasCorrectPageNumbers()
+        {
+            var items = Enumerable.Range(1, 10);
+            var list = new PagedList<int>(items, 10);
+            Assert.Equal(1, list.NumPages);
+
+            items = Enumerable.Range(1, 20);
+            list = new PagedList<int>(items, 20, pageSize: 20);
+            Assert.Equal(1, list.NumPages);
+
+            list = new PagedList<int>(items, 21, pageSize: 20);
+            Assert.Equal(2, list.NumPages);
+
+            list = new PagedList<int>(items, 99, pageSize: 20);
+            Assert.Equal(5, list.NumPages);
+        }
+
+        [Fact]
+        public void HandlesIncorrectPage()
+        {
+            var items = Enumerable.Range(1, 20);
+            var list = new PagedList<int>(items, 20, pageSize: 20, pageNumber: 2);
+            Assert.Equal(1, list.NumPages);
+        }
+
+        [Fact]
+        public void HasCorrectOffset()
+        {
+            var items = Enumerable.Range(1, 20);
+            var list = new PagedList<int>(items, 20, pageSize: 20, pageNumber: 1);
+            Assert.Equal(0, list.Offset);
+
+            list = new PagedList<int>(items, 50, pageSize: 20, pageNumber: 2);
+            Assert.Equal(20, list.Offset);
+
+            list = new PagedList<int>(items, 50, pageSize: 25, pageNumber: 2);
+            Assert.Equal(25, list.Offset);
+        }
+
+        [Fact]
+        public void HasCorrectStartEndNumbers()
+        {
+            var items = Enumerable.Range(1, 20);
+            var list = new PagedList<int>(items, 20, pageSize: 20, pageNumber: 1);
+            Assert.Equal(1, list.StartItem);
+            Assert.Equal(20, list.EndItem);
+
+            list = new PagedList<int>(items, 50, pageSize: 20, pageNumber: 2);
+            Assert.Equal(21, list.StartItem);
+            Assert.Equal(40, list.EndItem);
+
+            list = new PagedList<int>(items, 50, pageSize: 25, pageNumber: 2);
+            Assert.Equal(26, list.StartItem);
+            Assert.Equal(50, list.EndItem);
+
+            list = new PagedList<int>(items, 57, pageSize: 20, pageNumber: 3);
+            Assert.Equal(41, list.StartItem);
+            Assert.Equal(57, list.EndItem);
+
+            list = new PagedList<int>(items, 41, pageSize: 20, pageNumber: 3);
+            Assert.Equal(41, list.StartItem);
+            Assert.Equal(41, list.EndItem);
+
+            list = new PagedList<int>(items, 13, pageSize: 20, pageNumber: 1);
+            Assert.Equal(1, list.StartItem);
+            Assert.Equal(13, list.EndItem);
+
+            list = new PagedList<int>(new int[0], 0, pageSize: 20, pageNumber: 1);
+            Assert.Equal(0, list.StartItem);
+            Assert.Equal(0, list.EndItem);
+
+            list = new PagedList<int>(new int[] { 1 }, 1, pageSize: 20, pageNumber: 1);
+            Assert.Equal(1, list.StartItem);
+            Assert.Equal(1, list.EndItem);
+
+        }
+
+
+    }
+}
