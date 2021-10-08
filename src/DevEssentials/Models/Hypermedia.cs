ffd94fcfe12cd2e535e1d88dc5c3c494bf335798
Dave Thieben <davethieben@filevine.com>
2021-10-08 12:10:37 -0400

    clean up hypermedia structure

--- src/DevEssentials/Models/Hypermedia.cs
+++ src/DevEssentials/Models/Hypermedia.cs
@@ -12,31 +12,37 @@ namespace Essentials.Models
         }
 
         public T? Model { get; set; }
+    
+        public new Hypermedia<T> AddLink(string name, string url, string method = "GET", string? rel = null)
+        {
+            Links.Add(name, new HypermediaLink(url, rel, method));
+            return this;
+        }
     }
 
     public class Hypermedia
     {
-        public List<HypermediaLink> Links { get; set; } = new List<HypermediaLink>();
+        public Dictionary<string, HypermediaLink> Links { get; set; } = new Dictionary<string, HypermediaLink>();
         public List<KeyValuePair<string, string>> Errors { get; set; } = new List<KeyValuePair<string, string>>();
 
-        public Hypermedia AddLink(string url, string method = "GET", string? rel = null)
+        public Hypermedia AddLink(string name, string url, string method = "GET", string? rel = null)
         {
-            Links.Add(new HypermediaLink(url, rel, method));
+            Links.Add(name, new HypermediaLink(url, rel, method));
             return this;
         }
     }
 
     public class HypermediaLink
     {
-        public HypermediaLink(string url, string? rel = null, string? method = null, string? type = null)
+        public HypermediaLink(string href, string? rel = null, string? method = null, string? type = null)
         {
-            Url = url;
+            Href = href;
             Rel = rel;
             if (method != null)
                 Method = method;
         }
 
-        public string Url { get; set; } = default!;
+        public string Href { get; set; } = default!;
         public string? Rel { get; set; }
         public string Method { get; set; } = "GET";
         public string? Type { get; set; }
