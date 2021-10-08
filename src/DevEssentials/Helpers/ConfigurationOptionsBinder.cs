48c4b6b6d292f64a2141288a6e3a1e59d74f1c13
Dave Thieben <davethieben@filevine.com>
2021-10-08 12:10:24 -0400

    add helper to bind configuration options

---
+++ src/DevEssentials/Helpers/ConfigurationOptionsBinder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Essentials.Configuration
+{
+    public class ConfigurationOptionsBinder<TOptions> : IConfigureOptions<TOptions>
+        where TOptions : class
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationOptionsBinder(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(TOptions options)
+        {
+            ConfigurationBinder.Bind(_configuration, options);
+        }
+
+    }
+}
