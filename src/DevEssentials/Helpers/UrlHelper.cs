cb5a7f9d1e1ab32ac981f8009f98ebb3af973824
Dave Thieben <davethieben@filevine.com>
2021-10-27 17:06:42 -0400

    add GetSafeUrl

--- src/DevEssentials/Helpers/UrlHelper.cs
+++ src/DevEssentials/Helpers/UrlHelper.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Essentials
 {
     public static class UrlHelper
     {
         /// <summary>
-        /// tests is a string URL is safe for local redirect
+        /// tests if a string URL is safe for local redirect
         /// </summary>
-        public static bool IsRelative(string input)
+        public static bool IsRelative(
+#if NETSTANDARD2_1_OR_GREATER
+            [NotNullWhen(true)] 
+#endif
+            string? input)
         {
             if (!string.IsNullOrEmpty(input)
                 && Uri.TryCreate(input, UriKind.RelativeOrAbsolute, out Uri result)
@@ -15,6 +20,17 @@ namespace Essentials
                 return true;
 
             return false;
+        }
+
+#if NETSTANDARD2_1_OR_GREATER
+        [return: NotNull]
+#endif
+        public static string GetSafeUrl(string? input, string? @default = "/")
+        {
+            if (input != null && IsRelative(input))
+                return input;
+            else
+                return @default ?? string.Empty;
         }
 
     }
