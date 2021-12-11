7f8bcfcc733c74198c796165b1806fbd0411713f
Dave Thieben <davethieben@filevine.com>
2021-12-11 11:59:36 -0500

    Adding Base36 utils

---
+++ src/DevEssentials/Helpers/Base36.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Essentials
+{
+    public static class Base36
+    {
+        private const string _alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+        /// <summary>
+        /// https://codereview.stackexchange.com/a/14101
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="bigEndian"></param>
+        /// <returns></returns>
+        public static string Encode(byte[] bytes)
+        {
+            int estLength = (int)Math.Ceiling(bytes.Length * 8 / Math.Log(36, 2));
+
+            var dividend = new BigInteger(bytes);
+            var builder = new List<char>(estLength);
+            while (dividend != 0)
+            {
+                BigInteger remainder;
+                dividend = BigInteger.DivRem(dividend, 36, out remainder);
+                int index = Math.Abs((int)remainder);
+                builder.Add(_alphabet[index]);
+            }
+            builder.Reverse();
+            return new string(builder.ToArray());
+        }
+
+        public static byte[] Decode(string input)
+        {
+            BigInteger product = new BigInteger(0);
+            foreach (char c in input)
+            {
+                int index = _alphabet.IndexOf(c);
+                product = BigInteger.Add(product, BigInteger.Multiply(index, 36));
+            }
+            return product.ToByteArray();
+        }
+
+
+    }
+}
---
+++ test/DevEssentials.Test/Helpers/Base36Tests.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Xunit;
+
+namespace Essentials.Test.Helpers
+{
+    public class Base36Tests
+    {
+
+
+        [Fact]
+        public void Encode01()
+        {
+            var input = new byte[] { 1 };
+
+            string output = Base36.Encode(input);
+
+            Assert.NotEmpty(output);
+            Assert.Equal("1", output);
+        }
+
+        [Fact]
+        public void Encode02()
+        {
+            var input = new byte[] { 1, 2, 3, 4 };
+
+            string output = Base36.Encode(input);
+
+            Assert.NotEmpty(output);
+            Assert.Equal("142lmp", output);
+        }
+
+        [Theory]
+        [InlineData("a")]
+        [InlineData("one1")]
+        [InlineData("The Quick Brown Fox Jumped Over The Lazy Brown Dogs!@#$^&*?")]
+        public void OutputSameAsInput(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+
+            string encoded = Base36.Encode(bytes);
+
+            bytes = Base36.Decode(encoded);
+            string output = Encoding.UTF8.GetString(bytes);
+
+            Assert.Equal(input, output);
+        }
+
+    }
+}
