e69e3de4552bd6d3ade22631e3f585cbc549ee10
Dave Thieben <davethieben@filevine.com>
2021-12-11 15:00:47 -0500

    add ServiceFactory for testing

---
+++ src/DevEssentials.Testing/ServiceFactory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using Essentials;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace DevEssentials.Testing
+{
+    /// <summary>
+    /// ServiceFactory is a test helper for creating and configuring a dependency injection
+    /// container to use when testing service classes with lots of dependencies. 
+    /// </summary>
+    public static class ServiceFactory
+    {
+        public static ServiceFactorySetup Create(params object[] singletonInstances)
+        {
+            var services = new ServiceFactorySetup();
+
+            if (!singletonInstances.IsNullOrEmpty())
+            {
+                foreach (var instance in singletonInstances)
+                {
+                    if (instance != null)
+                        services.AddSingleton(instance.GetType(), instance);
+                }
+            }
+
+            return services;
+        }
+
+        public static ServiceFactorySetup GenerateFor<T>(params object[] singletonInstances)
+            where T : class
+        {
+            return Create(singletonInstances)
+                .AddImplementation<T>();
+        }
+
+        public static IServiceCollection AddDependencyMocks(this IServiceCollection services, Type implType)
+        {
+            var constructors = implType.GetConstructors().OrderBy(c => c.GetParameters().Length);
+            foreach (var constructor in constructors)
+            {
+                System.Reflection.ParameterInfo[] parameters = constructor.GetParameters();
+                if (parameters.Any(p => !p.ParameterType.IsInterface))
+                    continue;
+
+                foreach (var parameter in parameters)
+                {
+                    if (!services.Contains(parameter.ParameterType))
+                    {
+                        services.AddMock(parameter.ParameterType);
+                    }
+                }
+
+                break;
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddMock<T>(this IServiceCollection services, Action<Mock<T>>? setup = null)
+            where T : class
+        {
+            services.IsRequired();
+            services.AddMock(typeof(T));
+
+            var mock = services.FirstOrDefault(sd => sd.ServiceType == typeof(Mock<T>))?.ImplementationInstance as Mock<T>;
+            if (mock != null && setup != null)
+                setup.Invoke(mock);
+
+            return services;
+        }
+
+        public static IServiceCollection AddMock(this IServiceCollection services, Type serviceType)
+        {
+            var mockType = typeof(Mock<>);
+            var implMockType = mockType.MakeGenericType(serviceType);
+            var mockConstruct = implMockType.GetConstructor(new Type[] { });
+            var mock = mockConstruct.Invoke(null) as Mock;
+
+            services.AddSingleton(implMockType, mock); // add Mock<TService>
+            services.AddTransient(serviceType, sp => mock.Object); // add TService
+
+            return services;
+        }
+    
+    }
+}
---
+++ src/DevEssentials.Testing/ServiceFactorySetup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using Essentials;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace DevEssentials.Testing
+{
+    /// <summary>
+    /// helper class for setting up <see cref="ServiceFactory"/>
+    /// </summary>
+    public class ServiceFactorySetup : ServiceCollection
+    {
+        public ServiceFactorySetup()
+        {
+        }
+
+        /// <summary>
+        /// configure and set up the <see cref="Mock{TService}"/> thats been added to the container
+        /// </summary>
+        /// <typeparam name="TService">service Type to configure the Mock</typeparam>
+        /// <param name="mockSetup">delegate to perform the configuration</param>
+        /// <returns>itself</returns>
+        public ServiceFactorySetup Setup<TService>(Action<Mock<TService>> mockSetup)
+            where TService : class
+        {
+            var mockDescriptor = this.FirstOrDefault(sd => sd.ServiceType == typeof(Mock<TService>));
+            if (mockDescriptor == null)
+            {
+                this.AddMock<TService>();
+
+                mockDescriptor = this.FirstOrDefault(sd => sd.ServiceType == typeof(Mock<TService>));
+            }
+
+            var mock = mockDescriptor.ImplementationInstance as Mock<TService>;
+            if (mock == null)
+                throw new ApplicationException($"Mock could not be created or registered: {typeof(Mock<TService>)}");
+
+            mockSetup(mock);
+            return this;
+        }
+
+        public ServiceFactorySetup AddImplementation<TImpl>()
+            where TImpl : class
+        {
+            this.Remove<TImpl>();
+
+            // extension for MVC:`
+            //if (typeof(System.Web.Mvc.Controller).IsAssignableFrom(typeof(TImpl)))
+            //{
+            //    this.AddController(typeof(TImpl));
+            //}
+            //else
+            {
+                this.AddTransient<TImpl>();
+            }
+
+            this.AddDependencyMocks(typeof(TImpl));
+
+            return this;
+        }
+
+        public ServiceFactorySetup AddImplementation<TService, TImpl>()
+            where TService : class
+            where TImpl : class, TService
+        {
+            this.Remove<TService>();
+            this.Remove<Mock<TService>>();
+
+            this.AddTransient<TService, TImpl>();
+            this.AddDependencyMocks(typeof(TImpl));
+
+            return this;
+        }
+
+    }
+}
