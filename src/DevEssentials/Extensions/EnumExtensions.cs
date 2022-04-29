60b7e18c467c4a24721b09368347e7a844def142
Dave Thieben <davethieben@filevine.com>
2022-04-29 12:13:20 -0400

    enhance enum GetCustomAttributes

--- src/DevEssentials/Extensions/EnumExtensions.cs
+++ src/DevEssentials/Extensions/EnumExtensions.cs
@@ -24,17 +24,35 @@ namespace Essentials
         /// <typeparam name="TAttribute">Type of the Attribute to try to get</typeparam>
         /// <param name="value">value to look for Attributes on</param>
         /// <returns>the Attribute if found, otherwise null</returns>
-        public static TAttribute? GetEnumValueAttribute<TAttribute>(object value) where TAttribute : Attribute
+        public static Attribute[] GetCustomAttributes<TEnum>(this TEnum value)
+            where TEnum : struct, IConvertible
         {
             Contract.IsRequired(value, nameof(value));
 
             Type type = value.GetType();
             if (!type.IsEnum)
-                throw new ArgumentException($"Type {type} is not an enum");
+                throw new ArgumentException($"Type '{type}' must be an enum");
 
             // Get the enum field.
-            FieldInfo field = type.GetField(value.ToString());
-            return field?.GetCustomAttribute<TAttribute>();
+            FieldInfo? field = type.GetField(value.ToString(CultureInfo.InvariantCulture));
+            if (field != null)
+            {
+                return Attribute.GetCustomAttributes(field);
+            }
+            return Array.Empty<Attribute>();
+        }
+
+        public static TAttr? GetCustomAttribute<TEnum, TAttr>(this TEnum value)
+            where TEnum : struct, IConvertible
+            where TAttr : Attribute
+        {
+            return value.GetCustomAttributes().OfType<TAttr>().FirstOrDefault();
+        }
+
+        public static object? GetCustomAttribute<TEnum>(this TEnum value, Type attrType)
+            where TEnum : struct, IConvertible
+        {
+            return value.GetCustomAttributes().FirstOrDefault(a => attrType.IsAssignableFrom(a.GetType()));
         }
 
         public static string GetDescription<TEnum>(this TEnum value) where TEnum : struct, IComparable, IFormattable, IConvertible
